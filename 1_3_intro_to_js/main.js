47aeab6765495b1ad232fcd2fb685ca78acdedf7
rubesc <ruby.w.chu@gmail.com>
2023-09-08 18:24:17 -0400

    added assignment 1.2

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,14 @@
 console.log('hello world');
+
+const string = "string";
+const number=3;
+const string_not_boolean = "false";
+const array = [1,2,3] 
+const object = {
+    key: "value",
+    key: "value",
+}
+
+
+const thing = 1;
+const thingTwo = 2;
